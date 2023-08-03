2ed48d84b7bfb2afab7fc2a128896de564140570
Atul Kumar Tiwari <atulkumartiwari@Atuls-MacBook-Air.local>
2023-08-03 09:48:17 +0530

    basic program

--- personal_details.cpp
+++ personal_details.cpp
@@ -4,7 +4,7 @@ int main(){
     int reg_no = 12221533;
     char name[10] = "Atul";
     float marks = 0.01;
-    double phone_no = 123456789;
+    long long int phone_no = 123456789;
     cout << reg_no << endl;
     cout << name << endl;
     cout << marks << endl;
