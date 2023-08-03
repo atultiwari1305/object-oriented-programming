73e879c0b0252fff35a82fb90dcd4adbb4162a17
Atul Kumar Tiwari <atulkumartiwari@Atuls-MacBook-Air.local>
2023-08-03 09:40:35 +0530

    personal details

---
+++ personal_details.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int reg_no = 12221533;
+    char name[10] = "Atul";
+    float marks = 0.01;
+    double phone_no = 123456789;
+    cout << reg_no << endl;
+    cout << name << endl;
+    cout << marks << endl;
+    cout << phone_no << endl;
+}
