bd56b2d216acbf98391f7d42042e83c9d7a7323c
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-08-16 09:09:30 +0530

    array of structure

--- arrayOfStruct.cpp
+++ arrayOfStruct.cpp
@@ -9,13 +9,15 @@ struct emp
 int main()
 {
     emp e[4];
+    //The size of array defined here is dynamic it can even increase its size if
+    //the iteration of for loop is greater than the given size
     int i;
-    for(i=0;i<=2;i++)
+    for(i=0;i<10;i++)
     {
         cout<<"Enter the name of the employee with id number "<<i+1<<": ";
         cin.getline(e[i].name,20);
     }
-    for(i=0;i<=2;i++)
+    for(i=0;i<10;i++)
     {
         cout<<"The name of employee at number "<<i+1<<" is: "<<e[i].name<<endl;
     }
